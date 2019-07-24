bc2ba6e0f5f4c2281b6d85360234dcd69d7fa8a5
fionatqh <fionatqh@yahoo.com>
2019-07-24 19:40:00 +0800

    added Product class

---
+++ src/main/java/sales/products/Product.java
@@ -0,0 +1,50 @@
+package sales.products;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+	private Long id;
+	private String name;
+	private String brand;
+	private String madein;
+	private float price;
+
+public Product() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getBrand() {
+	return brand;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+public String getMadein() {
+	return madein;
+}
+public void setMadein(String madein) {
+	this.madein = madein;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+
+
+}
