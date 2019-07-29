86e10bb8512555bad8053692411b5f7d2172630f
fionatqh <fionatqh@yahoo.com>
2019-07-29 23:26:52 +0800

    added rest controller

--- src/main/java/sales/products/ProductManagerApplication.java
+++ src/main/java/sales/products/ProductManagerApplication.java
@@ -2,6 +2,12 @@ package sales.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ProductManagerApplication {
@@ -9,5 +15,13 @@ public class ProductManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagerApplication.class, args);
 	}
-
+	
+	@RestController
+	class GreetingController {
+	    
+	    @RequestMapping("/hello/{name}")
+	    String hello(@PathVariable String name) {
+	        return "Hello, " + name + "!";
+	    }
+	}
 }
