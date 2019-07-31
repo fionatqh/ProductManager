692389718c520e0f255a215968f1f7d3dcf2e23e
fionatqh <fionatqh@yahoo.com>
2019-07-31 20:41:21 +0800

    added home, new_product pages

--- src/main/java/sales/products/AppController.java
+++ src/main/java/sales/products/AppController.java
@@ -2,10 +2,13 @@ package sales.products;
 
 import java.util.List;
 
+import org.hibernate.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AppController {
@@ -16,8 +19,19 @@ public class AppController {
 	public String viewHomePage(Model model) {
 		List<Product> listProducts = ps.listAll();
 		model.addAttribute("listProducts",listProducts);
-		
-		
-		return "Index";
+		return "Home";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductForm(Model model) {
+		Product product = new Product();
+		model.addAttribute("product",product);
+		return "New_Product";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		ps.save(product);
+		return "redirect:/";
 	}
 }
